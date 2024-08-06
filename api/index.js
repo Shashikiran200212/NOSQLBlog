1efe5cc6b4f3a71a55069d4ce44a0c4faf677190
Shashikiran200212 <134192630+Shashikiran200212@users.noreply.github.com>
2024-08-06 14:33:32 +0530

    new updates1.6

--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
+// api/index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('../models/article');
 const articleRouter = require('../routes/articles');
 const methodOverride = require('method-override');
-require('dotenv').config(); // Ensure dotenv is required here
-
 const app = express();
 
-console.log('Connecting to MongoDB...');
-mongoose.connect(process.env.MONGODB_CONNECT_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_CONNECT_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -23,7 +25,4 @@ app.get('/', async (req, res) => {
 
 app.use('/articles', articleRouter);
 
-// Vercel serverless function handler
-module.exports = (req, res) => {
-  app(req, res);
-};
+module.exports = app;
